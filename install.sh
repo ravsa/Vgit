9c1a7f3b733f4a7f26a6b7544777e0e2dfa85960
ravsa <rvsbana@gmail.com>
2015-08-29 01:39:27 +0530

    install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+if vim --version | grep "+python" >> /dev/null
+then
+   while true
+   do
+      if which expect>>/dev/null
+      then
+         cp -rvf vimrc ~/.vimrc
+         cp -rvf vim ~/.vim
+         cp -rvf cheat_sheet.txt ~/
+      else
+         echo 'Do you want to install it? [Y/N]'
+         read ans
+         if [ "$ans" = 'y' -o "$ans" = 'Y' ]
+         then
+            sudo apt-get install expect
+         else
+            break;
+         fi
+      fi
+   done
+
+else
+   echo "ERROR: This plugin is not compatible with current version of vim"
+fi
