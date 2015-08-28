4dcd63693aa86ec67b058741f99d58caf1efb74f
ravsa <rvsbana@gmail.com>
2015-08-29 00:46:46 +0530

    update setup module for color

--- vim/setup.py
+++ vim/setup.py
@@ -1,13 +1,15 @@
 import pickle,getpass
-import os,subprocess
+import os,subprocess,color
 home_dir=subprocess.Popen('echo ~',shell=True,stdout=subprocess.PIPE)
 home_dir=home_dir.stdout.read()[:-1]
 creds=home_dir+'/'+'.creds'
 gitconfig=home_dir+'/'+'.gitconfig'
 def setup():
     data1=[]
-    data1.append(raw_input("Enter email address: "))
-    data1.append(raw_input("Enter Username : "))
+    color.printf('whitebg','black','Email    :')
+    data1.append(raw_input())
+    color.printf('whitebg','black','Username :')
+    data1.append(raw_input())
     os.system('git config --global user.email '+'"'+data1[0]+'"')
     os.system('git config --global user.name '+'"'+data1[1]+'"')
 try:
@@ -28,8 +30,9 @@ try:
 except Exception,e:
     file=open(creds,'wb')
     data2=[]
-    data2.append(raw_input("Enter username: "))
-    data2.append(getpass.getpass("Enter Password: "))
-    print data2
+    color.printf('whitebg','black','Username :')
+    data2.append(raw_input())
+    color.printf('whitebg','red','Password :')
+    data2.append(getpass.getpass(''))
     pickle.dump(data2,file)
     file.close()
